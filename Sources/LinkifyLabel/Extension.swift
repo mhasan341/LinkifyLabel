8530de8d2260aa8be44522cf3214dfda46989b27
Mahmudul Hasan <mhasan341@gmail.com>
2023-08-27 02:29:29 +0600

    Added Code

--- Sources/LinkifyLabel/Extension.swift
+++ Sources/LinkifyLabel/Extension.swift
@@ -1,8 +1,98 @@
 //
-//  File.swift
+//  Extension.swift
 //  
 //
 //  Created by Mahmudul Hasan on 2023-08-26.
 //
 
-import Foundation
+import UIKit
+
+extension LinkifyLabel {
+    /// Creates and returns a LinkifyLabel
+    /// - Parameters:
+    ///   - alignment: alignment of your text. Default: .left
+    ///   - inText: your label text
+    ///   - links: Keys are words you'd like to highlight as link, values you'll get by tapping those links
+    ///   - defaultStyle: settings false returns a red link, more in future
+    ///   - onTap: callback function where you'll get the tapped value as defined in values
+    /// - Returns: LinkifyLabel
+    static func linkify(withAlignment alignment: NSTextAlignment = .left, inText: String, links: Dictionary<String, String>, defaultStyle: Bool = true, onTap: @escaping (String) -> Void) -> LinkifyLabel {
+        let attributedString = NSMutableAttributedString(string: inText)
+
+        links.forEach({ link in
+            let linkAttribute: NSAttributedString.Key = defaultStyle ? .link : .linkRed
+            let attributes: [NSAttributedString.Key: Any] = [
+                linkAttribute: link.value
+            ]
+            let urlAttributedString = NSAttributedString(string: link.key, attributes: attributes)
+
+            let range = (attributedString.string as NSString).range(of: link.key)
+
+            attributedString.replaceCharacters(in: range, with: urlAttributedString)
+
+        })
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = alignment
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+
+        let label = LinkifyLabel()
+        label.attributedText = attributedString
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.didTapOnURL = onTap
+
+        return label
+
+    }
+
+
+    /// Creates and returns a LinkifyLabel
+    /// - Parameters:
+    ///   - alignment: alignment of your text. Default: .left
+    ///   - inText: your label text
+    ///   - linkWords: words you'd like to highlight as link
+    ///   - values: values you'll get by tapping those links
+    ///   - defaultStyle: settings false returns a red link, more in future
+    ///   - onTap: callback function where you'll get the tapped value as defined in values
+    /// - Returns: LinkifyLabel
+    static func linkify(withAlignment alignment: NSTextAlignment = .left, inText: String, linkWords: [String], values: [String], defaultStyle: Bool = true, onTap: @escaping (String) -> Void) -> LinkifyLabel {
+        // we want a pair for each
+        assert(linkWords.count == values.count, "linkWords and values must be equal in count. You have \(linkWords.count) linkWords and \(values.count) values")
+        let attributedString = NSMutableAttributedString(string: inText)
+
+        // since link item and values are equal in length we can just iterate over one items length
+        for index in 0..<linkWords.count {
+            // the linked word that user sees
+            let linkText = linkWords[index]
+            // the linked value
+            let linkValue = values[index]
+            // if our main string doesn't contain this value we'll skip
+            if !inText.contains(linkText) { continue }
+
+            let linkAttribute: NSAttributedString.Key = defaultStyle ? .link : .linkRed
+
+            let attributes: [NSAttributedString.Key: Any] = [
+                linkAttribute: linkValue
+            ]
+
+            let urlAttributedString = NSAttributedString(string: linkText, attributes: attributes)
+
+            let range = (attributedString.string as NSString).range(of: linkText)
+
+            attributedString.replaceCharacters(in: range, with: urlAttributedString)
+        }
+
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = alignment
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+
+        let label = LinkifyLabel()
+        label.attributedText = attributedString
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.didTapOnURL = onTap
+
+        return label
+    }
+}
+
--- Sources/LinkifyLabel/LinkifyLabel.swift
+++ Sources/LinkifyLabel/LinkifyLabel.swift
@@ -1,6 +1,106 @@
-public struct LinkifyLabel {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+final class LinkifyLabel: UILabel {
+
+    // MARK: Creating the Label
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        numberOfLines = 0
+        isUserInteractionEnabled = true
+    }
+
+    convenience init(textAlignment alignment: NSTextAlignment = .left) {
+        self.init(frame: .zero)
+        self.textAlignment = alignment
+    }
+
+    private var linkRedAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.systemRed]
+
+    override var attributedText: NSAttributedString? {
+        get {
+            return super.attributedText
+        }
+        set {
+            super.attributedText = {
+                guard let newValue = newValue else { return nil }
+                let text = NSMutableAttributedString(attributedString: newValue)
+
+                text.enumerateAttribute(.linkRed, in: NSRange(location: 0, length: text.length), options: .longestEffectiveRangeNotRequired) { (value, subrange, _) in
+                    guard let _ = value else { return }
+                    text.addAttributes(linkRedAttributes, range: subrange)
+                }
+
+                return text
+            }()
+        }
+    }
+
+    // MARK: Finding Hyperlink Under Touch
+
+    var didTapOnURL: (String) -> Void = {_ in}
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let url = self.url(at: touches) {
+            didTapOnURL(url)
+        }
+        else {
+            super.touchesEnded(touches, with: event)
+        }
+    }
+
+    private func url(at touches: Set<UITouch>) -> String? {
+        guard let attributedText = attributedText, attributedText.length > 0 else { return nil }
+        guard let touchLocation = touches.sorted(by: { $0.timestamp < $1.timestamp } ).last?.location(in: self) else { return nil }
+        guard let textStorage = preparedTextStorage() else { return nil }
+        let layoutManager = textStorage.layoutManagers[0]
+        let textContainer = layoutManager.textContainers[0]
+
+        let characterIndex = layoutManager.characterIndex(for: touchLocation, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        guard characterIndex >= 0, characterIndex != NSNotFound else { return nil }
+
+        // Glyph index is the closest to the touch, therefore also validate if we actually tapped on the glyph rect
+        let glyphRange = layoutManager.glyphRange(forCharacterRange: NSRange(location: characterIndex, length: 1), actualCharacterRange: nil)
+        let characterRect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
+        guard characterRect.contains(touchLocation) else { return nil }
+
+        // Custom link style
+        if let url = textStorage.attribute(.linkRed, at: characterIndex, effectiveRange: nil) as? String {
+            return url
+        }
+
+        return textStorage.attribute(.link, at: characterIndex, effectiveRange: nil) as? String
+    }
+
+    private func preparedTextStorage() -> NSTextStorage? {
+        guard let attributedText = attributedText, attributedText.length > 0 else { return nil }
+
+        // Creates and configures a text storage which matches with the UILabel's configuration.
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: bounds.size)
+        textContainer.lineFragmentPadding = 0
+        let textStorage = NSTextStorage(string: "")
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        textContainer.lineBreakMode = lineBreakMode
+        textContainer.size = textRect(forBounds: bounds, limitedToNumberOfLines: numberOfLines).size
+        textStorage.setAttributedString(attributedText)
+
+        return textStorage
+    }
+}
+
+
+extension NSAttributedString.Key {
+    static let linkRed = NSAttributedString.Key("linkRed")
 }
